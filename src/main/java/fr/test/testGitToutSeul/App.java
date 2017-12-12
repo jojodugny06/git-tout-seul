1e2cd2b7f8540d4d3e1d7a38cf705cd726f1840b
Joinel CHELEUX <lainy06@hotmail.fr>
2017-12-12 15:29:14 +0100

    Premier essai avec git

---
+++ src/main/java/fr/test/testGitToutSeul/App.java
@@ -0,0 +1,13 @@
+package fr.test.testGitToutSeul;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! C'est mon premier essai!!" );
+    }
+}
